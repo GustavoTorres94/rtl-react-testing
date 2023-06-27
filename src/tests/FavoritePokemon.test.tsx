9a227cbc6575968e80536c607f1ff05deaf43cea
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-06-26 23:11:01 -0300

    3p3

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -12,13 +12,13 @@ describe('testes no componente FavoritePokemon', () => {
   });
   it('testa se tem 1 ou mais pokemons favoritados', async () => {
     renderWithRouter(<App />, { route: '/pokemon/4' });
-      const isFavorite = screen.getByText(/pokémon favoritado\?/i);
-      await userEvent.click(isFavorite);
-      const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
-      await userEvent.click(favorite);
-      const pokemonOk = screen.getByText('Charmander');
-      const pokemonImg = screen.getByRole('img', { name: /charmander is marked as favorite/i });
-      expect(pokemonOk).toBeInTheDocument();
-      expect(pokemonImg).toBeInTheDocument();
+    const isFavorite = screen.getByText(/pokémon favoritado\?/i);
+    await userEvent.click(isFavorite);
+    const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    await userEvent.click(favorite);
+    const pokemonOk = screen.getByText('Charmander');
+    const pokemonImg = screen.getByRole('img', { name: /charmander is marked as favorite/i });
+    expect(pokemonOk).toBeInTheDocument();
+    expect(pokemonImg).toBeInTheDocument();
   });
 });
