cb3ba25f3098d2d45893e252c7b302c665b77075
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-06-26 22:55:20 -0300

    3p

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -1,1 +1,26 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('testes no componente FavoritePokemon', () => {
+  it('testa se não existem pokemons favoritados', () => {
+    renderWithRouter(<FavoritePokemon pokemonList={[]} />);
+    const noPokemonsText = screen.getByText('No favorite Pokémon found')
+    expect(noPokemonsText).toBeInTheDocument();
+  });
+  it('testa se tem 1 ou mais pokemons favoritados', async () => {
+    renderWithRouter(<App />); { route: '/pokemon/1' };
+    const detailsBtn = screen.getByRole('link', { name: /more details/i });
+    await userEvent.click(detailsBtn);
+    const isFavorite = screen.getByText(/pokémon favoritado\?/i);
+    await userEvent.click(isFavorite);
+    const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
+    await userEvent.click(favorite);
+    const pokemonOk = screen.getByText('Pikachu');
+    const pokemonImg = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(pokemonOk).toBeInTheDocument();
+    expect(pokemonImg).toBeInTheDocument();
+  });
+});
