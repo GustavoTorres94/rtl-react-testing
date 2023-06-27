da11b8ad638fffdec8565ac7fdaba1fc9f19fe9e
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-06-26 23:22:43 -0300

    4p

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,17 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('testes no componente NotFound', () => {
+  it('testa se tá na NotFound', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = screen.getByRole('heading', { name: 'Page requested not found' });
+    expect(notFoundText).toBeInTheDocument();
+  });
+  it('testa se tem iamgem no NotFound', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
