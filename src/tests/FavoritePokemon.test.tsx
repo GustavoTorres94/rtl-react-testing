3363b80a3085ef19a7336c0a795fec3674921e35
GustavoTorres94 <gustavo.t.paula@gmail.com>
2023-06-26 23:09:25 -0300

    3p2

--- src/tests/FavoritePokemon.test.tsx
+++ src/tests/FavoritePokemon.test.tsx
@@ -6,21 +6,19 @@ import App from '../App';
 
 describe('testes no componente FavoritePokemon', () => {
   it('testa se não existem pokemons favoritados', () => {
-    renderWithRouter(<FavoritePokemon pokemonList={[]} />);
-    const noPokemonsText = screen.getByText('No favorite Pokémon found')
+    renderWithRouter(<FavoritePokemon pokemonList={ [] } />);
+    const noPokemonsText = screen.getByText('No favorite Pokémon found');
     expect(noPokemonsText).toBeInTheDocument();
   });
   it('testa se tem 1 ou mais pokemons favoritados', async () => {
-    renderWithRouter(<App />); { route: '/pokemon/1' };
-    const detailsBtn = screen.getByRole('link', { name: /more details/i });
-    await userEvent.click(detailsBtn);
-    const isFavorite = screen.getByText(/pokémon favoritado\?/i);
-    await userEvent.click(isFavorite);
-    const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
-    await userEvent.click(favorite);
-    const pokemonOk = screen.getByText('Pikachu');
-    const pokemonImg = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
-    expect(pokemonOk).toBeInTheDocument();
-    expect(pokemonImg).toBeInTheDocument();
+    renderWithRouter(<App />, { route: '/pokemon/4' });
+      const isFavorite = screen.getByText(/pokémon favoritado\?/i);
+      await userEvent.click(isFavorite);
+      const favorite = screen.getByRole('link', { name: /favorite pokémon/i });
+      await userEvent.click(favorite);
+      const pokemonOk = screen.getByText('Charmander');
+      const pokemonImg = screen.getByRole('img', { name: /charmander is marked as favorite/i });
+      expect(pokemonOk).toBeInTheDocument();
+      expect(pokemonImg).toBeInTheDocument();
   });
 });
